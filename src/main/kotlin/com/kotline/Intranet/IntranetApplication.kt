e610d535d25756e8cc486cce7795abb569b92562
Nenuphare <caye.damien@gmail.com>
2022-11-21 11:18:59 +0100

    Création

---
+++ src/main/kotlin/com/kotline/Intranet/IntranetApplication.kt
@@ -0,0 +1,11 @@
+package com.kotline.Intranet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IntranetApplication
+
+fun main(args: Array<String>) {
+	runApplication<IntranetApplication>(*args)
+}
---
+++ src/test/kotlin/com/kotline/Intranet/IntranetApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotline.Intranet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IntranetApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
