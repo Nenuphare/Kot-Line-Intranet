96c73698fdc4d36d16f023dcd56ea06ec9348aa8
Nenuphare <caye.damien@gmail.com>
2022-12-08 14:53:49 +0100

    Controller et index

---
+++ src/main/kotlin/com/kotline/Intranet/controller/MainController.kt
@@ -0,0 +1,25 @@
+package com.kotline.Intranet.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Controller
+class MainController {
+
+    @GetMapping("/")
+    fun index(model: Model): String {
+        model["title"] = "Hello world !"
+        return "main/index"
+    }
+
+
+
+
+
+
+}
